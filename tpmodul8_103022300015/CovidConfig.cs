8c83e6a0987da7492f945f11051cdce7d39775c0
greengoblin8 <134504798+greengoblin8@users.noreply.github.com>
2025-04-14 01:24:14 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_103022300015/CovidConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace tpmodul8_103022300015
+{
+    public class CovidConfig
+    {
+        public string satuan_suhu { get; set; } = "celcius";
+        public int batas_hari_demam { get; set; } = 14;
+        public string pesan_ditolak { get; set; } = "anda tidak diperbolehkan masuk ke dalam gedung ini";
+        public string pesan_diterima { get; set; } = "anda dipersilahkan masuk ke dalam gedung ini";
+
+        private const string FilePath = "covid_config.json";
+
+        public static CovidConfig LoadConfig()
+        {
+            if (File.Exists(FilePath))
+            {
+                string json = File.ReadAllText(FilePath);
+                return JsonSerializer.Deserialize<CovidConfig>(json);
+            }
+            else
+            {
+                CovidConfig config = new CovidConfig();
+                config.SaveConfig();
+                return config;
+            }
+        }
+
+        public void SaveConfig()
+        {
+            string json = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(FilePath, json);
+        }
+
+        public void UbahSatuan()
+        {
+            satuan_suhu = (satuan_suhu == "celcius") ? "fahrenheit" : "celcius";
+            SaveConfig();
+        }
+    }
+}
--- tpmodul8_103022300015/Program.cs
+++ tpmodul8_103022300015/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tpmodul8_103022300015;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config =  CovidConfig.LoadConfig();
+
+        config.UbahSatuan();
+
+        Console.WriteLine($"satian suhu setelah diubah: {config.satuan_suhu}: ");
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}: ");
+        double suhu = double.Parse( Console.ReadLine() );
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        double hariDemam = double.Parse( Console.ReadLine() );
+
+        bool suhuValid = false;
+        if (config.satuan_suhu == "celcius")
+        {
+            suhuValid = suhu >= 36.5 && suhu <= 37.5;
+        }
+        else if (config.satuan_suhu == "fahrenheit") 
+        {
+            suhuValid = suhu >= 97.7 && suhu <= 99.5;
+        }
+
+        bool hariValid = hariDemam < config.batas_hari_demam;
+
+        if (suhuValid && hariValid)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        { 
+        Console.WriteLine(config.pesan_ditolak);
+        }
+    }
+}
